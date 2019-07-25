5c6467ae7eb2820a015f6f9b3795aeadff94c1a8
AdilZouitine <adilzouitinegm@gmail.com>
2019-07-25 13:44:28 +0200

    add requirements.txt :books:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 import outfit
 
-base_packages = ['peewee >=3.9.6', 'pandas>=0.24.2','tabular>=0.8.3']
+# base_packages = ['peewee>=3.9.6', 'pandas>=0.24.2','tabulate>=0.8.3']
 
 setup(name='outfit',
       version=outfit.__version__,
@@ -12,6 +12,6 @@ setup(name='outfit',
       author_email='adilzouitine@gmail.com',
       license='MIT',
       packages=find_packages(),
-      install_requires=base_packages,
+      # install_requires=base_packages,
       include_package_data=True,
       zip_safe=False)
