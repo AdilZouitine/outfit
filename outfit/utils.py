87b8ded3d2dae4429e3e12b329b0e6a2135be90a
sylvain.picard <sylvain.picard@irt-saintexupery.com>
2019-09-04 13:52:47 +0200

    Add RunGroup class, to keep an experiment group results

--- outfit/utils.py
+++ outfit/utils.py
@@ -1,7 +1,7 @@
 from functools import wraps
 import sys
 from typing import Callable, Any, NoReturn
-
+import matplotlib.pyplot as plt
 
 class Logger:
     '''Logger allowing to display on the console and write a message in a file
@@ -46,3 +46,22 @@ def getlog(filepath: str) -> Callable:
         return wrapper
 
     return decorator
+
+
+class RunGroup:
+    def __init__(self, df, on_param):
+        self.df = df
+        self.on_param = on_param
+        self.not_col = [self.on_param, "score", "experiment_name", "id_experiment"]
+        for column in self.df.columns:
+            if column not in self.not_col:
+                setattr(self, column, list(self.df[column])[0])
+
+    def plot(self, show=True):
+        scores = self.df.score
+        exp_name = self.df.experiment_name
+        param = self.df[self.on_param]
+        names = [self.on_param + "_" + p for p in param]
+        p = plt.bar(names, scores)
+        if show: plt.show()
+        return p
